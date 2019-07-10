b377605e7bf9d023b6295a2a90fa17db0bd6e8a3
tesschung <geobera0910@naver.com>
2019-07-10 17:08:09 +0900

    intro

--- startCamp/03_day/flask/intro/app.py
+++ startCamp/03_day/flask/intro/app.py
@@ -1,15 +1,21 @@
-from flask import Flask # module 호출
+from flask import Flask, render_template # module 호출
 import datetime
 import random
 app = Flask(__name__)
 
+# render_template(작성할 탬플릿 이름)
+
 @app.route('/') # @ == decorator, / == endpoint
 def hello(): # def 함수 만들기
-    return 'Hello SSAFY!!'
+    # return 'Hello SSAFY!!'
+    return render_template('index.html')  
+    # templates(반드시! templates)인 이름을 가진 폴더 안에서 index라는 html파일을 찾아 반환해준다
+
 
 @app.route('/ssafy')
 def ssafy():
     return 'Hello SSAFY!!!'
+
 
 @app.route('/dday')
 def day():
@@ -18,9 +24,11 @@ def day():
     td = b_day - today
     return f'{td.days}일 남았습니다.'
 
+
 @app.route('/html')
 def html():
     return '<h1> This is HTML h1 tag!</h1>'
+
 
 @app.route('/html_lines')
 def html_lines():
@@ -45,32 +53,33 @@ def greeting_Ziont():
 
 @app.route('/greeting/<name>') # http://127.0.0.1:5000/greeting/bts
 def greeting(name): # name == IU
-    return f'반갑습니다! {name} 님!'
+    return render_template('greeting.html', html_name=name) # name을 html_name으로 넘겨준다.
+    # 변수에 맞게 변환을 시켜주는 함수 -> jinja2
+
 
 @app.route('/cube/<int:number>')
 def cube(number):
-    return f'{number}의 3 제곱은 {number ** 3} 입니다.' #3제곱
+    result = number ** 3 # ** 앞뒤 공백 convention
+    return render_template('cube.html', html_result=result, html_number=number) # convention
+
 
 # 실습
 @app.route('/lunch/<int:people>')
-
 def lunch(people):
     # 사람수 만큼 메뉴 return
     menu = ['김밥', '짬뽕', '스시', '볶음밥']
     order = random.sample(menu,people)
     return str(order) 
 
-""" 실수:
-    new_choice = []
-    for a in range(people+1):    
-        new_choice.append(random.choice(menu))
-    return new_choice
-"""
+
+@app.route('/movie')
+def movie():
+    movies = ['스파이더맨', '앤드게임', '기생충', '알라딘']
+    movie = random.choice(movies)
+    return render_template('movie.html', html_movie=movie, html_movies=movies)
+
 
 # FLASK_APP=hello.py flask run
 # if __name__ == "__main__": import해서 사용하는 경우 or 직접 실행 경우 구분
-if __name__ == '__main__': # 접근용 모듈 생성
+if __name__ == '__main__': 
     app.run(debug=True) # debug=True 설정을 하여 정적이였던 것을 동적으로 변경
-
-
-
